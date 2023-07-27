e6513f46302b7d52787394f6499bbc221a2513c8
shanina12 <shaninavon123@gmail.com>
2023-07-27 11:28:52 +0300

    change app.js ->> app.jsx

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,6 @@ import { assert, describe, expect, it } from 'vitest'
 import { math } from './App'
 describe('APP', () =>{
   it('check calculator' , () => {
-  expect(math('3','100' , '*')).toEqual(300)
+  expect(math(3,8 , '*')).toEqual(3006)
   })
 })
